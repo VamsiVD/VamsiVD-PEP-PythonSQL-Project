b51e873bc954c2ffd273ba1e6eebd595929e63e9
VamsiVD <VamsiVD@github.com>
2026-07-17 22:52:06 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-17T22:52:06.067Z

--- src/main/main.py
+++ src/main/main.py
@@ -99,7 +99,13 @@ def load_and_clean_call_logs(file_path):
 # example: 1,105.0,4 - where 1 is the userId, 105.0 is the avgDuration, and 4 is the numCalls.
 def write_user_analytics(csv_file_path):
 
-    cursor.execute("SELECT userId, SUM(endTime - startTime AS avgDuration, COUNT(*) AS numCalls FROM callLogs GROUP BY userID")
+    cursor.execute("""
+    SELECT userId, 
+           SUM(endTime - startTime) AS totalDuration, 
+           COUNT(*) AS numCalls 
+    FROM callLogs 
+    GROUP BY userID
+    """)
     with open(csv_file_path, mode = "w") as file:
         writer = csv.writer(file)
         
