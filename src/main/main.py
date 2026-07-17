b1a5f23d103f892b1e66d4361e0de9ec68278eb8
VamsiVD <VamsiVD@github.com>
2026-07-17 22:39:22 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-17T22:39:22.365Z

--- src/main/main.py
+++ src/main/main.py
@@ -89,8 +89,7 @@ def load_and_clean_call_logs(file_path):
                 count +=1
 
 
-    #cursor.execute("SELECT * FROM callLogs")
-    print(cursor.fetchall())
+
 
 
 
@@ -98,7 +97,8 @@ def load_and_clean_call_logs(file_path):
 # You must save records consisting of each userId, avgDuration, and numCalls
 # example: 1,105.0,4 - where 1 is the userId, 105.0 is the avgDuration, and 4 is the numCalls.
 def write_user_analytics(csv_file_path):
-
+    cursor.execute("SELECT * FROM callLogs")
+    print(cursor.fetchall())
     cursor.execute("SELECT userId, COUNT(*) AS numCalls, SUM(endTime - startTime) / COUNT(*) AS avgDuration FROM callLogs GROUP BY userID")
     print(cursor.fetchall())
 
