3766f66bed2a24c8b8b3bf0a02f75b4fca372a1c
VamsiVD <VamsiVD@github.com>
2026-07-17 21:48:13 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-17T21:48:13.149Z

--- src/main/main.py
+++ src/main/main.py
@@ -68,7 +68,7 @@ def load_and_clean_users(file_path):
             count +=1
 
 
-    print("TODO: load_users")
+    print(cursor.fetchall())
 
 
 # This function will load the callLogs.csv file into the callLogs table, discarding any records with incomplete data
