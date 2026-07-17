15f98de94436df3db2ee5cd934f5cdb4c7d8700a
VamsiVD <VamsiVD@github.com>
2026-07-17 22:47:13 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-17T22:47:13.167Z

--- src/main/main.py
+++ src/main/main.py
@@ -100,7 +100,6 @@ def load_and_clean_call_logs(file_path):
 def write_user_analytics(csv_file_path):
 
     cursor.execute("SELECT userId, COUNT(*) AS numCalls, SUM(endTime - startTime) / COUNT(*) AS avgDuration FROM callLogs GROUP BY userID")
-    print(cursor.fetchall())
     with open(csv_file_path, mode = "w") as file:
         writer = csv.writer(file)
         
