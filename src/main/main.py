e0ff02af427f3006efaf4282fe7a217981fe3828
VamsiVD <VamsiVD@github.com>
2026-07-17 23:00:34 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-17T23:00:34.802Z

--- src/main/main.py
+++ src/main/main.py
@@ -101,7 +101,7 @@ def write_user_analytics(csv_file_path):
 
     cursor.execute("""
     SELECT userId, 
-           FLOOR(SUM(endTime - startTime) / COUNT()) AS totalDuration, 
+           (SUM(endTime - startTime) / COUNT() * 1.0) AS totalDuration, 
            COUNT(*) AS numCalls 
     FROM callLogs 
     GROUP BY userID
