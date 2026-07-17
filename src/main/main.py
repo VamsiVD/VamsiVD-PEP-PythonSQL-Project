a977fbddd32a77e3aca50e916a30400cbc0c0afe
VamsiVD <VamsiVD@github.com>
2026-07-17 22:55:34 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-17T22:55:34.848Z

--- src/main/main.py
+++ src/main/main.py
@@ -101,10 +101,11 @@ def write_user_analytics(csv_file_path):
 
     cursor.execute("""
     SELECT userId, 
-           SUM(endTime - startTime) / COUNT(*) AS totalDuration, 
+           SUM(endTime - startTime) / COUNT() AS totalDuration, 
            COUNT(*) AS numCalls 
     FROM callLogs 
     GROUP BY userID
+    ORDER BY totalDuration DESC
     """)
     data = cursor.fetchall()
     print(data)
