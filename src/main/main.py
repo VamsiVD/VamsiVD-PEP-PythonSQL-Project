8d021bbdb9ec61d141a06223155a8ec62f1e42e1
VamsiVD <VamsiVD@github.com>
2026-07-17 23:57:23 +0000

    Test run: 3/4 passed, 1 failed, 0 skipped - 2026-07-17T23:57:23.798Z

--- src/main/main.py
+++ src/main/main.py
@@ -127,7 +127,6 @@ def write_ordered_calls(csv_file_path):
     print(data)
     with open(csv_file_path, mode = "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["userId", "startTime"])
         writer.writerows(data)
 
     print("TODO: write_ordered_calls")
